c7b935345d6255343ec9e8cc12b9e945f319cf0d
Ian Sears <ian.shane.sears@gmail.com>
2022-03-12 16:17:21 -0700

    Adds ability to add dynamic amount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import TriggerButton from './comonents/TriggerButton';
+import shyaboi from './sounds/shyaboi.mp3'
 
 function App() {
+
+  const [numberOfButtons, setNumberOfButtons] = useState(9)
+  const [buttonassignment, setButtonAssignment] = useState([])
+  const [buttState, setButtState] = useState([])
+
+
+  // useEffect(() => {
+
+  //   const res = [...Array(numberOfButtons)].map((_, i) => {
+  //     buttonassignment.push(i)
+  //     return i;
+  //   });
+  // }, [])
+
+  const plusOne = ()=>{
+    setNumberOfButtons(numberOfButtons + 1)
+  }
+
+  let index = 0
+  let thing = []
+  useEffect(()=>{
+      while(index < numberOfButtons) {
+        index++
+        thing.push(<TriggerButton soundz={shyaboi} label={'shyaboi mp3'} />)
+      }
+      setButtState(thing)
+  },[buttState, numberOfButtons])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // button should have a SETUP mode to set the sound, the trigger key/midi channel
+    <>
+    <h2>
+      Number of Buttons: {numberOfButtons}
+    </h2>
+      <button className="h-12 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-blue-800" onClick={plusOne} >+1</button>
+    <div className="grid place-items-center grid-cols-3 h-screen">
+    {buttState.map(butt => {
+    return (
+      butt
+    )
+  })}
+      {/* <TriggerButton soundz={shyaboi} label={'shyaboi mp3'} /> */}
     </div>
+    </>
   );
 }
 
---
+++ src/comonents/TriggerButton/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { Howl, Howler } from 'howler';
+
+const playThing = (e) => {
+  let soundz = e.target.getAttribute('data-sound');
+  var sound = new Howl({
+    src: [soundz]
+  });
+
+  sound.play();
+}
+
+
+function TriggerButton({ soundz, label, keyAssined }) {
+  
+  useEffect(() => {
+    const listener = event => {
+      if (event.code) {
+        console.log(event.code);
+        event.preventDefault();
+        // callMyFunction();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+
+    // button should have a SETUP mode to set the sound, the trigger key/midi channel
+    // TODO add control section to change pitch, add effects
+    <>
+      <button className="h-12 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" data-sound={soundz} onClick={playThing} buttonAssignment={keyAssined} >{label}</button>
+    </>
+  );
+}
+
+export default TriggerButton;
