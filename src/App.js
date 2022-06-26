24d77fe486aab5135bdfddd600d216cbf6e4a000
Ian Sears <ian.shane.sears@gmail.com>
2022-06-26 00:04:03 -0400

    Adds input upload and base64 file conversion

--- src/App.js
+++ src/App.js
@@ -5,48 +5,79 @@ import shyaboi from './sounds/shyaboi.mp3'
 
 function App() {
 
-  const [numberOfButtons, setNumberOfButtons] = useState(9)
+  const [numberOfButtons, setNumberOfButtons] = useState(6)
   const [buttonassignment, setButtonAssignment] = useState([])
   const [buttState, setButtState] = useState([])
 
-
-  // useEffect(() => {
-
-  //   const res = [...Array(numberOfButtons)].map((_, i) => {
-  //     buttonassignment.push(i)
-  //     return i;
-  //   });
-  // }, [])
-
-  const plusOne = ()=>{
+  const plusOne = () => {
     setNumberOfButtons(numberOfButtons + 1)
   }
 
+  function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      console.log(reader.result);
+      localStorage.setItem('mp3,1', reader.result);
+    };
+  }
+
+
+  function getFile(e) {
+    let file = e.target.files[0];
+    getBase64(file);
+  }
+
+
   let index = 0
   let thing = []
-  useEffect(()=>{
-      while(index < numberOfButtons) {
-        index++
-        thing.push(<TriggerButton soundz={shyaboi} label={'shyaboi mp3'} />)
-      }
-      setButtState(thing)
-  },[buttState, numberOfButtons])
+
+  useEffect(() => {
+    while (index < numberOfButtons) {
+      index++
+      thing.push(<TriggerButton soundz={shyaboi} label={'shyaboi mp3'} />)
+    }
+    setButtState(thing)
+  }, [numberOfButtons])
 
   return (
     // button should have a SETUP mode to set the sound, the trigger key/midi channel
     <>
-    <h2>
-      Number of Buttons: {numberOfButtons}
-    </h2>
-      <button className="h-12 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-blue-800" onClick={plusOne} >+1</button>
-    <div className="grid place-items-center grid-cols-3 h-screen">
-    {buttState.map(butt => {
-    return (
-      butt
-    )
-  })}
-      {/* <TriggerButton soundz={shyaboi} label={'shyaboi mp3'} /> */}
-    </div>
+      <h2>
+        Number of Buttons: {numberOfButtons}
+      </h2>
+      <button className="h-12 px-9 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-blue-800" onClick={plusOne} >+1</button>
+      <div className="flex justify-center">
+        <div className="mb-3 w-96">
+          <label for="formFile" className="form-label inline-block mb-2 text-gray-700">Default file input example</label>
+          <input className="form-control
+            block
+            w-full
+            px-3
+            py-1.5
+            text-base
+            font-normal
+            text-gray-700
+            bg-white bg-clip-padding
+            border border-solid border-gray-300
+            rounded
+            transition
+            ease-in-out
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            type="file"
+            id="formFile"
+            onChange={getFile}
+          />
+        </div>
+      </div>
+      <div className="grid place-items-center grid-cols-6 h-screen">
+        {buttState.map(butt => {
+          return (
+            butt
+          )
+        })}
+      </div>
     </>
   );
 }
--- src/comonents/TriggerButton/index.jsx
+++ src/comonents/TriggerButton/index.jsx
@@ -32,7 +32,7 @@ function TriggerButton({ soundz, label, keyAssined }) {
     // button should have a SETUP mode to set the sound, the trigger key/midi channel
     // TODO add control section to change pitch, add effects
     <>
-      <button className="h-12 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" data-sound={soundz} onClick={playThing} buttonAssignment={keyAssined} >{label}</button>
+      <button className="h-20 px-20 m-3 text-xl text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800" data-sound={soundz} onClick={playThing} buttonAssignment={keyAssined} >{label}</button>
     </>
   );
 }
